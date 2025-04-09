f1a9e4ecb325cb4a5efacea97bfd63cc1e1cf757
Akkirala vishwanth <vishwanth5595@gmail.com>
2025-04-09 10:50:24 +0530

    first commit

---
+++ esm.js
@@ -0,0 +1,5 @@
+// import { vishwanth } from "./functions.js";
+// console.log(vishwanth(12, 6));
+import { vishwanth } from "./functions.js";
+
+console.log(vishwanth(6, 60));
---
+++ functions.js
@@ -0,0 +1,16 @@
+// export const modull = function(a, b){
+//       return a + b;
+// }
+// console.log(modull(2, 6))
+
+// export const modules = (a, b) => console.log(10 + a + b +5)
+// modules(2, 10);
+
+// export const vishwa = num1 => console.log(10 + num1);
+// vishwa(16);
+
+// export var vishu = (a, b) => a * b;
+// console.log(vishu(12, 35));
+
+// export const vishwanth = (para1, para2) => para1 + para2;
+export const vishwanth = (para1, para2) => para1 % para2;
